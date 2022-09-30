e58d81500442eb7a0af9e7d9069ee36ed9d0e3bf
Guillem <guillemgovi@gmail.com>
2022-10-01 01:10:25 +0200

    Classe 113

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from "../../interfaces/por-pais.interface";
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-capital',
@@ -8,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorCapitalComponent implements OnInit {
 
-  constructor() { }
+  termino: string = '';
+  isError: boolean = false;
+  capitals: Country[] = [];
+
+  constructor(private paisService: PaisService) { }
 
   ngOnInit(): void {
+  }
+
+  buscar(termino: string) {
+    this.isError = false;
+    this.termino = termino;
+    console.log(this.termino);
+
+    this.paisService.buscarCapital(this.termino)
+      .subscribe(resp => {
+        this.capitals = resp;
+        console.log(resp);
+      }, err => {
+        this.isError = true;
+      });
+  }
+
+  sugerencias(termino: string) {
+    this.buscar(termino);
   }
 
 }
