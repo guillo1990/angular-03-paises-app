0499314f79a80dfef58f5b04d38f73b133823afb
Guillem <guillemgovi@gmail.com>
2022-10-01 11:57:27 +0200

    Classe 115

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-ver-pais',
@@ -14,15 +15,22 @@ export class VerPaisComponent implements OnInit {
 
   ngOnInit(): void {
     this.activatedRoute.params
-      .subscribe(params => {
-        console.log(params);
+      .pipe(
+        switchMap((params) => this.paisService.getPaisPorAlpha(params['id']))
+      )
+      .subscribe(resp => {
+        console.log(resp);
+    })
+    // this.activatedRoute.params
+    //   .subscribe(params => {
+    //     console.log(params);
 
-        this.paisService.getPaisPorAlpha(params['id']).subscribe(pais => {
-          console.log(pais);
+    //     this.paisService.getPaisPorAlpha(params['id']).subscribe(pais => {
+    //       console.log(pais);
 
-        })
+    //     })
 
-      } )
+    //   } )
   }
 
 }
