8843c35f2a2dd18cbad7d8264ee4d69312cf00bb
Guillem <guillemgovi@gmail.com>
2022-10-01 12:24:23 +0200

    Classe 116

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/por-pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -11,15 +12,22 @@ import { switchMap } from 'rxjs/operators';
 })
 export class VerPaisComponent implements OnInit {
 
+  pais!: Country[];
+
   constructor( private activatedRoute: ActivatedRoute, private paisService: PaisService ) { }
 
   ngOnInit(): void {
     this.activatedRoute.params
       .pipe(
-        switchMap((params) => this.paisService.getPaisPorAlpha(params['id']))
+        switchMap((params) => this.paisService.getPaisPorAlpha(params['id'])),
+        tap(pais => {
+          this.pais = pais;
+          console.log("1",this.pais);
+        })
       )
-      .subscribe(resp => {
-        console.log(resp);
+      .subscribe(pais => {
+        this.pais = pais;
+        console.log(2,pais);
     })
     // this.activatedRoute.params
     //   .subscribe(params => {
