f7e93d2b9fd9e1f03e25deef179a12cbc6a083af
Guillem <guillemgovi@gmail.com>
2022-10-03 16:16:07 +0200

    Classe 124

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Country } from '../../interfaces/por-pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
@@ -13,10 +16,38 @@ export class PorRegionComponent{
 
   regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
   regionActiva: string = '';
+  countries: Country[] = [];
+  isError: boolean = false;
+  loading: boolean = false;
 
-  constructor() { }
+  constructor(private paisService: PaisService) { }
+
+  getClassCSS(region: string) {
+    if (region === this.regionActiva) {
+      return 'btn btn-primary';
+    }
+    else {
+      return 'btn btn-outline-primary';
+    }
+  }
 
   activarRegion(region: string) {
+    if (region === this.regionActiva) {
+      return;
+    }
     this.regionActiva = region;
+    this.buscarRegion(region);
+  }
+
+  buscarRegion(region: string) {
+    this.loading = true;
+    this.paisService.getPaisPorRegion(region)
+      .subscribe(resp => {
+        this.countries = resp;
+        this.loading = false;
+      }, err => {
+        this.isError = true;
+        this.loading = false;
+      });
   }
 }
