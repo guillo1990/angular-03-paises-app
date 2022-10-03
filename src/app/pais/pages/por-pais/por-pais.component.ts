82d7efc589224e04bc763104da69f0ae91d91970
Guillem <guillemgovi@gmail.com>
2022-10-03 17:03:59 +0200

    Classe 128

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,6 +6,11 @@ import { PaisService } from '../../services/pais.service';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `
   ]
 })
 export class PorPaisComponent {
@@ -13,25 +18,38 @@ export class PorPaisComponent {
   termino: string = '';
   isError: boolean = false;
   countries: Country[] = [];
+  suggeredCountries: Country[] = [];
 
   constructor(private paisService: PaisService) { }
 
   buscar(termino: string) {
     this.isError = false;
     this.termino = termino;
-    console.log(this.termino);
 
     this.paisService.buscarPais(this.termino)
       .subscribe(resp => {
         this.countries = resp;
-        console.log(resp);
       }, err => {
         this.isError = true;
       });
   }
 
   sugerencias(termino: string) {
+    this.isError = false;
+    this.termino = termino;
+    this.suggeredCountries = [];
+
+    this.paisService.buscarPais(termino)
+      .subscribe(resp => {
+        this.suggeredCountries = resp.splice(0,3);
+      }, err => {
+        this.suggeredCountries = [];
+      });
+  }
+
+  buscarSugerido(termino: string) {
     this.buscar(termino);
+    this.suggeredCountries = [];
   }
 
 }
